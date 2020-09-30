f7a9317eebaed94eeaa16f52b2d8b3e38f68fefd
Sprova <sprova_03@gmail.com>
2020-09-30 19:37:14 -0300

    Docs agregado

---
+++ docs/precache-manifest.212ad0ea792ca22eefe95a1311216229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037cbbbf370f6fe3a2cea043ccd0a96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1bf97847c384780f48c",
+    "url": "/static/css/main.e265cdf7.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bf97847c384780f48c",
+    "url": "/static/js/main.e6b07191.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
